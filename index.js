a99b34c71f660cc0f4c265ba98bcb4a5dd34a05f
shaniawaggu <121105618+shaniawaggu@users.noreply.github.com>
2025-01-29 01:30:54 +0000

    Add .env to project

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()  //
+
 const app = require('./app')
-const port = 3000 //define the port variable
+const port = process.env.PORT //define the port variable . this was prev 3000
 
-
+//want to replace this js variable with environmetn variable
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
  })
